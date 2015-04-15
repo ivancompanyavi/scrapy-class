81a6385be3370f6d0aa89350d009fdbb76623b8b
Ivan Compañy <ivan.company@mytaste.com>
2015-04-15 12:18:46 +0200

    - Reformated some code to be read better in Presentation Mode of PyCharm

--- scrapyclass/scrapyclass/spiders/seriesly.py
+++ scrapyclass/scrapyclass/spiders/seriesly.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapyclass.items import SerieslyItem
 
+
 class SerieslySpider(scrapy.Spider):
 
     name = 'seriesly'
@@ -12,16 +13,19 @@ class SerieslySpider(scrapy.Spider):
 
     def start_requests(self):
         return [scrapy.FormRequest('http://series.ly/scripts/login/login.php',
-                                   formdata={'lg_login': self.username, 'lg_pass': self.password},
+                                   formdata={'lg_login': self.username,
+                                             'lg_pass': self.password},
                                    callback=self.parse_main_page)]
 
     def parse_main_page(self, response):
-        return scrapy.Request('http://series.ly/@' + self.username + '/_series', callback=self.parse_profile)
+        url = 'http://series.ly/@' + self.username + '/_series'
+        return scrapy.Request(url, callback=self.parse_profile)
 
     def parse_profile(self, response):
         profile_uid = response.css('#profileUID').xpath('@value').extract()[0]
         yield scrapy.Request(
-            'http://series.ly/perfil/subperfil/series.php?uid={}&id={}'.format(profile_uid, profile_uid),
+            'http://series.ly/perfil/subperfil/series.php?uid={}&id={}'
+            .format(profile_uid, profile_uid),
             callback=self.parse_series)
 
     def parse_series(self, response):
--- scrapyclass/scrapyclass/spiders/twitter.py
+++ scrapyclass/scrapyclass/spiders/twitter.py
@@ -1,10 +1,8 @@
 import datetime
-
 import scrapy
-
 from scrapyclass.items import TwitterItem
 
-NUMBER_OF_TWEETS = 10
+MAX_TWEETS = 10
 
 
 class TwitterSpider(scrapy.Spider):
@@ -15,11 +13,11 @@ class TwitterSpider(scrapy.Spider):
         super(TwitterSpider, self).__init__()
 
     def parse(self, response):
-        tweets = response.xpath('//div[@data-component-term="tweet"]')
-        tweets = tweets[:NUMBER_OF_TWEETS] if len(tweets) <= NUMBER_OF_TWEETS else tweets
+        tweets = response.xpath('//div[@data-component-term="tweet"]')[:MAX_TWEETS]
         for tweet in tweets:
             item = TwitterItem()
             item['tweet'] = tweet.xpath('.//p/text()').extract()[0]
             date_timestamp = tweet.xpath('.//span[@data-time]/@data-time').extract()[0]
-            item['date'] = datetime.datetime.fromtimestamp(int(date_timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+            tweet_date = datetime.datetime.fromtimestamp(int(date_timestamp))
+            item['date'] = tweet_date.strftime('%Y-%m-%d %H:%M:%S')
             yield item
